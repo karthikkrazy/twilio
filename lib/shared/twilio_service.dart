04fda06c0ac461e719c3cbafbaee18c463b9acf8
Karthik Kothagattu <kothagattukarthik24@gmail.com>
2022-12-12 14:45:34 +0530

    first commit

---
+++ lib/shared/twilio_service.dart
@@ -0,0 +1,26 @@
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class TwilioFunctionsService {
+  TwilioFunctionsService._();
+  static final instance = TwilioFunctionsService._();
+
+  final http.Client client = http.Client();
+  final accessTokenUrl =
+      "https://twiliochatroomaccesstoken-9922.twil.io/accessToken";
+
+  Future<dynamic> createToken(String identity) async {
+    try {
+      Map<String, String> header = {
+        'Content-Type': 'application/json',
+      };
+      var url = Uri.parse(accessTokenUrl + '?user=' + identity);
+      final response = await client.get(url, headers: header);
+      Map<String, dynamic> responseMap = jsonDecode(response.body);
+      return responseMap;
+    } catch (error) {
+      throw Exception([error.toString()]);
+    }
+  }
+}
